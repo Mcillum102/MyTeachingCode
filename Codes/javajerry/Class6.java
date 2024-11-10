f7046e4e17040fb8106f86a750e2fcff09c7c1ee
Mcillum102 <139927650+Mcillum102@users.noreply.github.com>
2024-11-10 15:33:16 -0800

    Basic of Arrays

--- Codes/javajerry/Class6.java
+++ Codes/javajerry/Class6.java
@@ -1,5 +1,7 @@
 package javajerry;
 
+import java.util.Arrays;
+
 public class Class6 {
     
     public static void main(String[] args) {
@@ -16,7 +18,28 @@ public class Class6 {
         numberArray[1] = 100;
         numberArray[2] = 90;
 
-        System.out.println();
+        // System.out.println(numberArray[1]); // print at index
 
-    }
+        // A for loop example to print each items out
+        for (int i = 0; i < numberArray.length; i++) {
+            System.out.println(numberArray[i] + " ");
+        }
+
+        /* For each loop
+         * 1. the variable you use to represent the items MUST be the same type as the arrayu
+         * This only works for arrays
+         */
+        for (int i : numberArray) {
+            System.out.println(i);
+        }
+
+
+        // Create an array using set
+        int[] intArray = {1,2,3,4,5};
+
+        String[] strArray = {"Hello", "nihao"};
+         
+        // We are not able to directly print array as it is in Python
+        System.out.println(intArray); // This will show the address of array.
+     }
 }
